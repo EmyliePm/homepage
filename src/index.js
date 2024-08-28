4f97dd094cfd7466bb18aff30ea9959e06a4b479
Emylie <167892046+EmyliePm@users.noreply.github.com>
2024-08-28 18:16:29 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+var introductionSkills = document.getElementById("introductionSkills");
+
+var typewriter = new Typewriter(introductionSkills, {
+  loop: true,
+  delay: 25,
+  deleteSpeed: 10,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString("I am a full-stack developer")
+  .pauseFor(1000)
+  .deleteChars(27)
+  .typeString("My main skills include:")
+  .pauseFor(1000)
+  .deleteChars(23)
+  .typeString("JS, HTML, CSS")
+  .pauseFor(1000)
+  .deleteChars(13)
+  .typeString("Python, C languages, Perl")
+  .pauseFor(1000)
+  .deleteChars(25)
+  .typeString("Hello there!")
+  .pauseFor(1000)
+  .deleteChars(12)
+  .pauseFor(1000)
+  .start();
